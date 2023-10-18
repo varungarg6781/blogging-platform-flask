60d280718fc82a7fe63e1fbb122dfd02bc1ac3a3
Restyled.io <commits@restyled.io>
2023-10-18 19:42:31 +0000

    Restyled by yapf

--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 class User(UserMixin):
     """ """
+
     def __init__(self, id, username, email, password):
         self.id = id
         self.username = username
@@ -15,21 +16,18 @@ users.append(
         username="user1",
         email="user1@example.com",
         password=generate_password_hash("password1", method="sha256"),
-    )
-)
+    ))
 users.append(
     User(
         id=2,
         username="user2",
         email="user2@example.com",
         password=generate_password_hash("password2", method="sha256"),
-    )
-)
+    ))
 users.append(
     User(
         id=3,
         username="user3",
         email="user3@example.com",
         password=generate_password_hash("password3", method="sha256"),
-    )
-)
+    ))
