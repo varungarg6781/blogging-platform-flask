d7e25fe6bcf660aa6ca38194cd7a3407d61f4f40
Restyled.io <commits@restyled.io>
2023-10-18 19:42:25 +0000

    Restyled by autopep8

--- models.py
+++ models.py
@@ -5,8 +5,12 @@ class User(UserMixin):
         self.email = email
         self.password = password
 
+
 # Dummy data for testing
-# just adding this comment o test 
-users.append(User(id=1, username='user1', email='user1@example.com', password=generate_password_hash('password1', method='sha256')))
-users.append(User(id=2, username='user2', email='user2@example.com', password=generate_password_hash('password2', method='sha256')))
-users.append(User(id=3, username='user3', email='user3@example.com', password=generate_password_hash('password3', method='sha256')))
+# just adding this comment o test
+users.append(User(id=1, username='user1', email='user1@example.com',
+             password=generate_password_hash('password1', method='sha256')))
+users.append(User(id=2, username='user2', email='user2@example.com',
+             password=generate_password_hash('password2', method='sha256')))
+users.append(User(id=3, username='user3', email='user3@example.com',
+             password=generate_password_hash('password3', method='sha256')))
