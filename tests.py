51db612e39e5098f06e4a28b028b814dc45db9d8
Restyled.io <commits@restyled.io>
2023-10-18 18:46:33 +0000

    Restyled by pyment

--- tests.py
+++ tests.py
@@ -4,25 +4,31 @@ from app import app
 
 
 class TestApp(unittest.TestCase):
+    """ """
     def setUp(self):
+        """ """
         app.config["TESTING"] = True
         self.app = app.test_client()
 
     def tearDown(self):
+        """ """
         pass
 
     def test_home_route(self):
+        """ """
         response = self.app.get("/")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"Welcome to My Blogging Platform", response.data)
 
     def test_posts_route(self):
+        """ """
         response = self.app.get("/posts")
         self.assertEqual(response.status_code, 200)
         self.assertIn(b"This is the first blog post.", response.data)
         self.assertIn(b"This is the second blog post.", response.data)
 
     def test_user_login(self):
+        """ """
         response = self.app.post(
             "/login",
             data=dict(email="user1@example.com", password="password123"),
@@ -36,12 +42,14 @@ class TestApp(unittest.TestCase):
     # Adding some more changes for PR to work
 
     def test_user_logout(self):
+        """ """
         response = self.app.get("/logout", follow_redirects=True)
         self.assertEqual(response.status_code, 200)  # Expect a redirect
         # Expect the user's email not to be in the response data
         self.assertNotIn(b"user1@example.com", response.data)
 
     def test_add_blog_post(self):
+        """ """
         with self.app:
             self.app.post(
                 "/login", data=dict(email="user1@example.com", password="password123")
